25edea5a65e8b776060c50a616f43a663f185144
pabloalban <pablo.alban@iess.gob.ec>
2022-09-26 09:01:15 -0500

    Eliminar archivos

--- R/RTR/106_lectura_situacion_actual_rtr.R
+++
@@ -1,6 +0,0 @@
-message( paste( rep('-', 100 ), collapse = '' ) )
-
-###########################################################################
-message( paste( rep('-', 100 ), collapse = '' ) )
-rm( list = ls()[ !( ls() %in% c( 'parametros' ) ) ] )
-gc()
